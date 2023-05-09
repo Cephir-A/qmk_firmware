537faa65613b5d5cf53bbe47e143f56b9806ca6c
violet.lawton <violacie22gmail.com>
2023-05-09 05:34:51 +0000

    Updates to Violacie keymap.

--- keyboards/lily58/keymaps/violacie/keymap.c
+++ keyboards/lily58/keymaps/violacie/keymap.c
@@ -1,5 +1,6 @@
 #include QMK_KEYBOARD_H
 
+
 enum layer_number {
   _QWERTY = 0,
   _LOWER,
@@ -27,9 +28,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT( \
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
-  KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI \
+                        MO(_LOWER), KC_LGUI, KC_LALT, KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -68,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT( \
-  KC__VOLUP, KC__VOLDOWN, KC_MEDIA_PLAY_PAUSE, KC_MFFD, KC_MRWD, KC_MUTE,                     KC_PSCR, KC_PGUP, KC_PGDN, KC_DEL, _______, _______, \
+  KC_VOLU, KC_VOLD, KC_MEDIA_PLAY_PAUSE, KC_MFFD, KC_MRWD, KC_MUTE,                     KC_PSCR, KC_PGUP, KC_PGDN, KC_DEL, _______, _______, \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
   _______,  KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                       KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT, XXXXXXX, XXXXXXX, \
   _______,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   KC_COPY, KC_PASTE,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
